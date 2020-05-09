f9ec010b5bbb84e9da58589911150722ec9318ff
kouki <kouworld0123@gmail.com>
2020-05-10 03:12:28 +0900

    :+1: docekrのAPIを叩けるようにした

--- pkg/server/service/service.go
+++ pkg/server/service/service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 
+	"github.com/docker/docker/client"
 	runtimeapi "k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 )
 
@@ -13,5 +14,15 @@ type DockerRuntimeServiceServer struct {
 
 func (c *DockerRuntimeServiceServer) Version(context.Context, *runtimeapi.VersionRequest) (*runtimeapi.VersionResponse, error) {
 	log.Println("リクエストがきたぞえ")
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.40"))
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	version, err := cli.ServerVersion(ctx)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(version)
 	return &runtimeapi.VersionResponse{}, nil
 }
